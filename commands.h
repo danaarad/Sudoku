537e2b3f9622688c398eaa1567394c7abe8e1333
yaelsapir <yaeltalyaofek@walla.com>
2018-08-11 13:55:10 +0300

    commands h

---
+++ commands.h
@@ -0,0 +1,23 @@
+/*
+ * commands.h
+ *
+ *  Created on: 11 баев 2018
+ *      Author: yael sapir
+ */
+
+#ifndef COMMANDS_H_
+#define COMMANDS_H_
+
+int isWin();
+
+int doExit();
+int doReset();
+int doAutofill();
+int doGetNumofSols();
+int doHint(int x, int y);
+int doSave(char *fileName);
+int doUndo();
+int doRedo();
+int doMarkErrors();
+
+#endif /* COMMANDS_H_ */
