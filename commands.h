5e17b043933f8ccea0749e6401e42ae3d56a102e
Dana Arad <danaarad4@gmail.com>
2018-09-01 10:13:19 +0300

    aaa

--- commands.h
+++ commands.h
@@ -9,18 +9,17 @@
 #define COMMANDS_H_
 #include "Game_structs.h"
 
-int doExit();
 int doReset();
-int doAutofill();
-int doGetNumofSols();
-int doHint(char *x, char *y);
-int doSave(char *fileName, mode_e mode);
+int doAutofill(Game *game);
+int doGetNumofSols(Game *game);
+int doHint(Game *game, char *x, char *y);
+int doSave(Game *game, char *fileName);
 int doEditFile(Game *game, char *fileName);
 int doSolveFile(Game *game, char *fileName);
 int doUndo();
 int doRedo();
 int doMarkErrors(Game *game, char *x);
-int doValidate();
+int doValidate(Game *game);
 int doSet(Game *game, char *x, char *y, char *z);
 int doGenerate(char *x, char *y);
 
