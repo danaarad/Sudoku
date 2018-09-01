a74c6406cff96c7d38be6ad70cf10b77a96a7d23
Dana Arad <danaarad4@gmail.com>
2018-09-01 10:11:59 +0300

    commands.h

--- commands.h
+++ commands.h
@@ -1,65 +1,27 @@
+/*
+ * commands.h
+ *
+ *  Created on:
+ *      Author: yael sapir
+ */
+
+#ifndef COMMANDS_H_
+#define COMMANDS_H_
 #include "Game_structs.h"
-#include "mainAux.h"
-#include "settings.h"
-#include "Game.h"
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 
-int main(int argc, char *argv[]) {
-	int exit = 0;
-	Game *game;
-	command_e command;
-	char  *x;
-	char  *y;
-	char  *z;
+int doExit();
+int doReset();
+int doAutofill();
+int doGetNumofSols();
+int doHint(char *x, char *y);
+int doSave(char *fileName, mode_e mode);
+int doEditFile(Game *game, char *fileName);
+int doSolveFile(Game *game, char *fileName);
+int doUndo();
+int doRedo();
+int doMarkErrors(Game *game, char *x);
+int doValidate();
+int doSet(Game *game, char *x, char *y, char *z);
+int doGenerate(char *x, char *y);
 
-	printf("Sudoku\n------\n");
-
-	/*while the user does not exit, start new game*/
-	while(!exit) {
-		game = (Game*) initGame(DEFAULT_BLOCK_HEIGHT, DEFAULT_BLOCK_WIDTH);
-		if (game == NULL){
-			return -1;
-		}
-		while (1) {
-			x = (char*) calloc(MAX_SIZE, sizeof(char));
-			y = (char*) calloc(MAX_SIZE, sizeof(char));
-			z = (char*) calloc(MAX_SIZE, sizeof(char));
-			if (x == NULL || y == NULL || z == NULL){
-				printf(CALLOC_ERROR);
-				return -1;
-			}
-			command = getCommand(game->mode, x, y, z);
-			printf("%s, %s, %s, %d \n", x, y, z, (int)command);
-			fflush(stdout);
-			if (command == exit_game) {
-				exit = 1;
-				break;
-			} else {
-				if (executeCommand(game, command, x, y, z) == -1){
-					return -1;
-				}
-				if (isWin()) {
-					freeGame(game);
-					break;
-				}
-			}
-		free(x);
-		free(y);
-		free(z);
-		}
-		if (exit == 1) {
-			free(x);
-			free(y);
-			free(z);
-			freeGame(game);
-			printf( "Exiting...");
-		}
-
-	}
-	return 1;
-}
-
-
-
+#endif /* COMMANDS_H_ */
