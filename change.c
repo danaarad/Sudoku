65d1105bf6598240aafe697308302cf96955d9f3
yaelsapir <yaeltalyaofek@walla.com>
2018-09-05 16:24:09 +0300

    print change

--- change.c
+++ change.c
@@ -37,3 +37,50 @@ void freeChanges(Change *change) {
 	}
 }
 
+
+int printChange(command_e command, actionType_e actionType, Change change){
+	int x, y, z1, z2;
+
+	if(actionType != SET_A){
+		return 0;
+	}
+
+	x = change->x;
+	y = change->y;
+
+	switch(command){
+		case undo:
+			printf("Undo ");
+			z1 = change->val_after;
+			z2 = change->val_before;
+			break;
+		case redo:
+			printf("Redo ");
+			z1 = change->val_before;
+			z2 = change->val_after;
+			break;
+		default:
+			return 0;
+	}
+	printf("%d,%d: ", x, y);
+
+	prinf("from ");
+	if(z1 == 0){
+		printf("_ ");
+	}else{
+		printf("%d ",z1);
+	}
+
+	prinf("to ");
+	if(z2 == 0){
+		printf("_ ");
+	}else{
+		printf("%d ",z2);
+	}
+
+	printf("\n");
+	fflush(stdout);
+	return 1;
+}
+
+
