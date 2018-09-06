fea87d46861a96a2109f3aee867eac0ee0af050e
Dana Arad <danaarad4@gmail.com>
2018-09-06 14:29:10 +0300

    a

--- arrayAux.h
+++ arrayAux.h
@@ -8,8 +8,8 @@
 #ifndef ARRAYAUX_H_
 #define ARRAYAUX_H_
 
-int filled_cells(int *arr);
-int get_first_value(int *arr);
+int filled_cells(int *arr, int len);
+int get_first_value(int *arr, int le);
 int findFirstFreeCellIn2DArr(int **Arr2D, int lenOfArr);
 
 #endif /* ARRAYAUX_H_ */
