d81cc07afecb1fc7f5c1cf5ad42eb44665234246
yaelsapir <yaeltalyaofek@walla.com>
2018-09-08 12:33:06 +0300

    print and mark errors

--- printer.c
+++ printer.c
@@ -44,7 +44,7 @@ void printBoard(Game* gp, valType_e valType) {
 						fflush(stdout);
 						if (getNodeValByType(gp, ISGIVEN, x, y) == 1 && mode != EDIT){
 							printf(".");
-						}else if(getNodeValByType(gp, ISERROR, x, y) == 1 && (markErrors || mode == EDIT)){
+						}else if(getNodeValByType(gp, ISERROR, x, y) == 1 && markErrors){
 							printf("*");
 						}else {
 							printf(" ");
