0a752116564997c0189c95cc2757845e8ef40e30
Dana Arad <danaarad4@gmail.com>
2018-09-15 22:29:31 +0300

    compiles!

--- Action.c
+++ Action.c
@@ -17,6 +17,7 @@
 #include <stdlib.h>
 #include "settings.h"
 #include "Action.h"
+#include "Game.h"
 
 /********************* SETTERS *********************/
 
--- printer.c
+++ printer.c
@@ -9,6 +9,7 @@
  */
 
 #include "printer.h"
+#include "Game.h"
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
